0eaf30278b8105c66c3b4eef6924a7216df9e2f3
moonwalker <dorin.tufar@freshcells.de>
2016-06-20 12:46:41 +0200

    removed unnecessary checks

--- src/main/java/tasks/DHT22Task.java
+++ src/main/java/tasks/DHT22Task.java
@@ -7,7 +7,7 @@ public class DHT22Task extends AbstractTask
 {
     public void run()
     {
-        DHT22 dht22 = (DHT22) this.getContainer().get("@DHT22");
+        DHT22 dht22 = (DHT22) this.get("@DHT22");
         dht22.read();
     }
 }
--- src/main/java/tasks/SymbiosisTask.java
+++ src/main/java/tasks/SymbiosisTask.java
@@ -5,7 +5,8 @@ import sensor.sensors.DHT22;
 
 public class SymbiosisTask extends AbstractTask
 {
-    public void run() {
+    public void run()
+    {
         DHT22 dht22 = (DHT22) this.get("@DHT22");
         System.out.println(dht22.getCurrent());
     }
